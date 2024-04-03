c0fa721d7be62df6a4c13575aac554d87192ee4b
getz <getz@sdf.org>
2024-04-03 15:42:37 +0200

    correct fg in modeline and font fix

--- mixins/vim-like.el
+++ mixins/vim-like.el
@@ -54,36 +54,39 @@
 
 (setq evil-insert-state-message nil)
 
-(defun turn-buffer-modeline-green ()
-  (set-face-foreground 'mode-line-inactive "white")
-  (set-face-foreground 'mode-line "#00ff00"))
+;; (defun turn-buffer-modeline-green ()
+;;   (set-face-foreground 'mode-line-inactive "white")
+;;   (set-face-foreground 'mode-line "#00ff00"))
 
-(defun turn-buffer-modeline-white ()
-  (set-face-foreground 'mode-line-inactive "white")
-  (set-face-foreground 'mode-line "#ffffff"))
+;; (defun turn-buffer-modeline-white ()
+;;   (set-face-foreground 'mode-line-inactive "white")
+;;   (set-face-foreground 'mode-line "#ffffff"))
 
-(defun turn-buffer-modeline-blue ()
-  (set-face-foreground 'mode-line-inactive "white")
-  (set-face-foreground 'mode-line "orange"))
+;; (defun turn-buffer-modeline-blue ()
+;;   (set-face-foreground 'mode-line-inactive "white")
+;;   (set-face-foreground 'mode-line "orange"))
 
-(defun do--status-bar-change-mode-line-color ()
-  (cond
-   ((memq evil-state '(hybrid insert emacs))
-    (turn-buffer-modeline-green))
-   ((memq evil-state '(visual))
-    (turn-buffer-modeline-blue))
-   (t
-    (turn-buffer-modeline-white))))
+;; (defun do--status-bar-change-mode-line-color ()
+;;   (cond
+;;    ((memq evil-state '(hybrid insert emacs))
+;;     (turn-buffer-modeline-green))
+;;    ((memq evil-state '(visual))
+;;     (turn-buffer-modeline-blue))
+;;    (t
+;;     (turn-buffer-modeline-white)
+;;     )
+;;    ))
 
-(add-hook 'post-command-hook 'do--status-bar-change-mode-line-color)
-(add-hook 'windmove-do-window-select 'do--status-bar-change-mode-line-color)
-(add-hook 'find-file-hook 'do--status-bar-change-mode-line-color)
-(add-hook 'dired-sidebar-mode-hook 'evil-normal-state)
+;; (add-hook 'post-command-hook 'do--status-bar-change-mode-line-color)
+;; (add-hook 'windmove-do-window-select 'do--status-bar-change-mode-line-color)
+;; (add-hook 'find-file-hook 'do--status-bar-change-mode-line-color)
+;; (add-hook 'dired-sidebar-mode-hook 'evil-normal-state)
 
 
-(defun dotspacemacs/user-config ()
-  (add-hook 'evil-hybrid-state-entry-hook 'turn-buffer-modeline-green)
-  (add-hook 'evil-hybrid-state-exit-hook 'turn-buffer-modeline-white))
+;; (defun dotspacemacs/user-config ()
+;;   (add-hook 'evil-hybrid-state-entry-hook 'turn-buffer-modeline-green)
+;;   (add-hook 'evil-hybrid-state-exit-hook 'turn-buffer-modeline-white)
+;;   )
 
 
 ;; Highlight yanked region
