4aad304db52fc65f10a2f3c404280be1ff646877
getz <getz@sdf.org>
2025-02-15 00:16:08 +0100

    projectile-ag deprecated atm

--- mixins/projectile.el
+++ mixins/projectile.el
@@ -47,31 +47,31 @@
 ;  :ensure t
 ;  )
 
-(use-package consult-ag
-  :ensure t
-  )
+;; (use-package consult-ag
+  ;; :ensure t
+  ;; )
 
-(defun my-consult-projectile-ag ()
-  "Run a consult-ag search in the project."
-  (interactive)
-  (require 'projectile)
-  (let* ((ignores (unless (eq (projectile-project-vcs) 'git)
-                    ;; ag supports git ignore files
-                    (append
-                     (projectile-ignored-files-rel) (projectile-ignored-directories-rel)
-                     (projectile--globally-ignored-file-suffixes-glob)
-                     grep-find-ignored-files grep-find-ignored-directories)))
-         (ignores-args (apply #'append
-                              (mapcar (lambda (item) (list "--ignore" item)) ignores))))
-    (funcall-interactively #'consult-ag
-                           (if-let ((s (symbol-at-point)))
-                               (symbol-name s)
-                             "")
-                           (projectile-project-root)
-                           ignores-args)))
+;; (defun my-consult-projectile-ag ()
+;;   "Run a consult-ag search in the project."
+;;   (interactive)
+;;   (require 'projectile)
+;;   (let* ((ignores (unless (eq (projectile-project-vcs) 'git)
+;;                     ;; ag supports git ignore files
+;;                     (append
+;;                      (projectile-ignored-files-rel) (projectile-ignored-directories-rel)
+;;                      (projectile--globally-ignored-file-suffixes-glob)
+;;                      grep-find-ignored-files grep-find-ignored-directories)))
+;;          (ignores-args (apply #'append
+;;                               (mapcar (lambda (item) (list "--ignore" item)) ignores))))
+;;     (funcall-interactively #'consult-ag
+;;                            (if-let ((s (symbol-at-point)))
+;;                                (symbol-name s)
+;;                              "")
+;;                            (projectile-project-root)
+;;                            ignores-args)))
 
-(let ((projectile-switch-project-action #'my-consult-projectile-ag)
-  (call-interactively #'projectile-switch-project)))
+;; (let ((projectile-switch-project-action #'my-consult-projectile-ag)
+;;   (call-interactively #'projectile-switch-project)))
 
 (use-package ag
   :ensure t)
